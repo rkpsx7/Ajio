48d86fa75ed8e5f912296f5990a72ac4beb6cd75
vipindevanz <83296815+vipindevanz@users.noreply.github.com>
2021-07-21 09:34:50 +0530

    Added Firebase authentication

--- app/src/main/java/com/example/ajio/activity/AccountActivity.java
+++ app/src/main/java/com/example/ajio/activity/AccountActivity.java
@@ -1,16 +1,98 @@
 package com.example.ajio.activity;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 import com.example.ajio.R;
+import com.example.ajio.databinding.ActivityAccountBinding;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
 
 public class AccountActivity extends AppCompatActivity {
+
+    private static final int SIGN_IN_KEY = 1;
+    private GoogleSignInClient mSignInClient;
+    private FirebaseAuth mAuth;
+    private FirebaseUser mUser;
+    private ActivityAccountBinding mBinding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_account);
+        mBinding = ActivityAccountBinding.inflate(getLayoutInflater());
+        setContentView(mBinding.getRoot());
+
+        mAuth = FirebaseAuth.getInstance();
+        mUser = mAuth.getCurrentUser();
+
+        mBinding.btnLogin.setOnClickListener(v->signInWithGoogle());
+    }
+
+    private void signInWithGoogle() {
+
+        GoogleSignInOptions gso = new GoogleSignInOptions
+                .Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+
+        mSignInClient = GoogleSignIn.getClient(this, gso);
+
+        Intent signInIntent = mSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, SIGN_IN_KEY);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == SIGN_IN_KEY) {
+
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+
+            try {
+
+                GoogleSignInAccount account = task.getResult(ApiException.class);
+
+                assert account != null;
+
+                firebaseAuthWithGoogle(account.getIdToken());
+
+            } catch (ApiException e) {
+
+                Toast.makeText(this, e.getMessage() + "", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    private void firebaseAuthWithGoogle(String idToken) {
+        AuthCredential credential = GoogleAuthProvider.getCredential(idToken, null);
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, task -> {
+                    if (task.isSuccessful()) {
+
+                        Toast.makeText(AccountActivity.this, "Success", Toast.LENGTH_SHORT).show();
+
+                        FirebaseUser user = mAuth.getCurrentUser();
+
+                    } else {
+
+                        Toast.makeText(AccountActivity.this, task.getException() + "", Toast.LENGTH_SHORT).show();
+                    }
+                });
     }
 }
--- app/src/main/java/com/example/ajio/activity/BagActivity.java
+++ app/src/main/java/com/example/ajio/activity/BagActivity.java
@@ -1,16 +1,125 @@
 package com.example.ajio.activity;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 import com.example.ajio.R;
+import com.example.ajio.databinding.ActivityBagBinding;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.GoogleAuthProvider;
 
 public class BagActivity extends AppCompatActivity {
+
+    private static final int SIGN_IN_KEY = 1;
+    private FirebaseAuth mAuth;
+    private ActivityBagBinding mBinding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bag);
+
+        mBinding = ActivityBagBinding.inflate(getLayoutInflater());
+        setContentView(mBinding.getRoot());
+
+        checkLoginState();
+
+        mAuth = FirebaseAuth.getInstance();
+
+        mBinding.ivCross.setOnClickListener(v -> finish());
+        mBinding.btnContinueShopping.setOnClickListener(v -> finish());
+        mBinding.btnLoginJoin.setOnClickListener(v -> signInWithGoogle());
+    }
+
+    private void signInWithGoogle() {
+
+        GoogleSignInOptions gso = new GoogleSignInOptions
+                .Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+
+        GoogleSignInClient signInClient = GoogleSignIn.getClient(this, gso);
+
+        Intent signInIntent = signInClient.getSignInIntent();
+        startActivityForResult(signInIntent, SIGN_IN_KEY);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == SIGN_IN_KEY) {
+
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+
+            try {
+
+                GoogleSignInAccount account = task.getResult(ApiException.class);
+
+                assert account != null;
+
+                firebaseAuthWithGoogle(account.getIdToken());
+
+            } catch (ApiException e) {
+
+                Toast.makeText(this, e.getMessage() + "", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    private void firebaseAuthWithGoogle(String idToken) {
+        AuthCredential credential = GoogleAuthProvider.getCredential(idToken, null);
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, task -> {
+                    if (task.isSuccessful()) {
+
+                        Toast.makeText(BagActivity.this, "Login Success", Toast.LENGTH_SHORT).show();
+
+                        saveLoginState();
+
+                    } else {
+
+                        Toast.makeText(BagActivity.this, task.getException() + "", Toast.LENGTH_SHORT).show();
+                    }
+                });
+    }
+
+    private void saveLoginState() {
+
+        SharedPreferences.Editor preferences = getSharedPreferences("PREFS", MODE_PRIVATE).edit();
+        preferences.putBoolean("loggedIn", true);
+        preferences.apply();
+
+        checkLoginState();
+    }
+
+    private void checkLoginState() {
+
+        SharedPreferences preferences = getSharedPreferences("PREFS", MODE_PRIVATE);
+        boolean loggedInAlready = preferences.getBoolean("loggedIn", false);
+
+        if (loggedInAlready) {
+
+            mBinding.btnLoginJoin.setVisibility(View.GONE);
+            mBinding.tvEmptyDescription.setVisibility(View.GONE);
+
+        } else {
+
+            mBinding.btnLoginJoin.setVisibility(View.VISIBLE);
+            mBinding.tvEmptyDescription.setVisibility(View.VISIBLE);
+        }
     }
 }
