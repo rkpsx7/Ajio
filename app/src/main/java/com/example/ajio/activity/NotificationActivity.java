a5c976023cce20a7f294a1d7ae466b286e704547
rkpsx7 <rkpsx7@gmail.com>
2021-07-21 18:58:19 +0530

    Notification done

--- app/src/main/java/com/example/ajio/activity/NotificationActivity.java
+++ app/src/main/java/com/example/ajio/activity/NotificationActivity.java
@@ -2,15 +2,23 @@ package com.example.ajio.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.example.ajio.R;
+import com.example.ajio.databinding.ActivityNavigationBinding;
+import com.example.ajio.databinding.ActivityNotificationBinding;
 
 public class NotificationActivity extends AppCompatActivity {
 
+    private ActivityNotificationBinding mBinding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_notification);
+        mBinding = ActivityNotificationBinding.inflate(getLayoutInflater());
+        setContentView(mBinding.getRoot());
+
+        mBinding.btnBack.setOnClickListener(v -> startActivity(new Intent(NotificationActivity.this, HomeActivity.class)));
+
     }
 }
