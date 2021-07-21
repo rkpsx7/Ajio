d761fe8d1d3703449855ccf1d2b4ed043bd60a91
vipindevanz <83296815+vipindevanz@users.noreply.github.com>
2021-07-21 16:00:17 +0530

    Updated WishlistActivity

--- app/src/main/java/com/example/ajio/activity/LoginActivity.java
+++ app/src/main/java/com/example/ajio/activity/LoginActivity.java
@@ -1,16 +1,101 @@
 package com.example.ajio.activity;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 import com.example.ajio.R;
+import com.example.ajio.databinding.ActivityLoginBinding;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.GoogleAuthProvider;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private static final int SIGN_IN_KEY = 1;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+
+        ActivityLoginBinding binding = ActivityLoginBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        mAuth = FirebaseAuth.getInstance();
+        binding.loginTemplate.setOnClickListener(v -> finish());
+        binding.imgGoogle.setOnClickListener(v -> signInWithGoogle());
+    }
+
+    private void signInWithGoogle() {
+
+        GoogleSignInOptions gso = new GoogleSignInOptions
+                .Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+
+        GoogleSignInClient signInClient = GoogleSignIn.getClient(this, gso);
+
+        Intent signInIntent = signInClient.getSignInIntent();
+        startActivityForResult(signInIntent, SIGN_IN_KEY);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == SIGN_IN_KEY) {
+
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+
+            try {
+
+                GoogleSignInAccount account = task.getResult(ApiException.class);
+
+                assert account != null;
+
+                firebaseAuthWithGoogle(account.getIdToken());
+
+            } catch (ApiException e) {
+
+                Toast.makeText(this, e.getMessage() + "", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    private void firebaseAuthWithGoogle(String idToken) {
+        AuthCredential credential = GoogleAuthProvider.getCredential(idToken, null);
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, task -> {
+                    if (task.isSuccessful()) {
+
+                        Toast.makeText(LoginActivity.this, "Login Success", Toast.LENGTH_SHORT).show();
+
+                        saveLoginState();
+
+                    } else {
+
+                        Toast.makeText(LoginActivity.this, task.getException() + "", Toast.LENGTH_SHORT).show();
+                    }
+                });
+    }
+
+    private void saveLoginState() {
+
+        SharedPreferences.Editor preferences = getSharedPreferences("PREFS", MODE_PRIVATE).edit();
+        preferences.putBoolean("loggedIn", true);
+        preferences.apply();
+
+        finish();
     }
 }
--- app/src/main/java/com/example/ajio/activity/WishlistActivity.java
+++ app/src/main/java/com/example/ajio/activity/WishlistActivity.java
@@ -1,16 +1,20 @@
 package com.example.ajio.activity;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
-import com.example.ajio.R;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.ajio.databinding.ActivityWishlistBinding;
 
 public class WishlistActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_wishlist);
+
+        ActivityWishlistBinding binding = ActivityWishlistBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.btnContinueShopping.setOnClickListener(v -> finish());
     }
 }
